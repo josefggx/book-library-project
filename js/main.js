e1b1de9d89594aeb368ede8307619e6242d313d8
Jose Gutierrez <josefgg58@gmail.com>
2022-06-09 18:29:04 -0500

    Changed the display function and added a clear one

--- js/main.js
+++ js/main.js
@@ -11,13 +11,6 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
 }
 
-let book1 = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
-
-const book2 = new Book("The Hobbit 2", "J.R.R. Tolkien", "295 pages", "not read yet");
-
-console.log(book1.info());
-
-console.log(book2.info());
 
 
 function addBookToLibrary(title, author, pages, read) {
@@ -25,23 +18,41 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(x);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien.", "295 pages", "Read");
-addBookToLibrary("The Hobbit3", "J.R.R. Tolkien.", "295 pages", "Not Read");
-addBookToLibrary("The Hobbit5", "J.R.R. Tolkien.", "295 pages", "Read");
-addBookToLibrary("The Hobbit3", "J.R.R. Tolkien.", "295 pages", "Not Read");
-addBookToLibrary("The Hobbit3", "J.R.R. Tolkien.", "295 pages", "Not Read");
-addBookToLibrary("The Hobbit3", "J.R.R. Tolkien.", "295 pages", "Not Read");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien.", "295", "Read");
+addBookToLibrary("The Hobbit2", "J.R.R. Tolkien.", "295", "Read");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien.", "295", "Read");
+addBookToLibrary("The Hobbit2", "J.R.R. Tolkien.", "295", "Read");
+
 
 
 const container = document.querySelector("#books-grid");
+const cards = document.querySelectorAll(".book-card")
+
+function clearContainer() {
+    container.innerHTML = "";
+    const card = document.createElement('div');
+    card.classList.add('invisible-card');
+    const card2 = document.createElement('div');
+    card2.classList.add('invisible-card');
+    const card3 = document.createElement('div');
+    card3.classList.add('invisible-card');
+    container.insertBefore(card, container.firstChild);
+    container.insertBefore(card2, container.firstChild);
+    container.insertBefore(card3, container.firstChild);
+}
 
 function displayBook() {
+    clearContainer();
     myLibrary.forEach(function (element) {
         const card = document.createElement('div');
         card.classList.add('book-card');
         // @ts-ignore
         card.dataset.index = myLibrary.indexOf(element);
         container.insertBefore(card, container.firstChild);
+        const data = document.createElement("p");
+        data.classList.add("data");
+        card.appendChild(data);
+        data.textContent = `DOM: ${card.dataset.index} | Array: ${myLibrary.indexOf(element)}`
         const title = document.createElement("p");
         title.classList.add("title");
         title.textContent = element.title;
@@ -52,7 +63,7 @@ function displayBook() {
         card.appendChild(author);
         const pages = document.createElement("p");
         pages.classList.add("pages");
-        pages.textContent = element.pages;
+        pages.textContent = `${element.pages} pages`;
         card.appendChild(pages);
         const read = document.createElement("button");
         read.classList.add("read");
@@ -65,47 +76,7 @@ function displayBook() {
     });
 }
 
-function displayStats() {
-    let myLibraryLength = myLibrary.length;
-    let myReadBooks = 0;
-    let myUnreadBooks = 0;
-    myLibrary.forEach(function (element) {
-        if(element.read === "Read") {
-            myReadBooks++;
-        } else {
-            myUnreadBooks++;
-        }
-    })
-    const readBooks = document.querySelector("#read");
-    readBooks.textContent = `Read: ${myReadBooks}`;
-    const unreadBooks = document.querySelector("#unread");
-    unreadBooks.textContent = `Unread: ${myUnreadBooks}`;
-    const totalBooks = document.querySelector("#total");
-    totalBooks.textContent = `Total: ${myLibraryLength}`;
-}
-
 displayBook();
-displayStats();
-
-const popup = document.querySelector(".popup");
-
-function formPopup() {
-    // @ts-ignore
-    popup.style.display = "block"
-}
-
-window.onclick = function (event) {
-    if(event.target == popup) {
-        // @ts-ignore
-        popup.style.display = "none";
-    }
-}
-
-// Working on this
-const readBtn = document.querySelector(".read")
-readBtn.addEventListener("click", () => {
-    readBtn.textContent = "Not Read";
-})
 
 const form = document.querySelector("#new-book-form");
 const submit = document.querySelector("#submit");
@@ -137,27 +108,88 @@ form.addEventListener('submit', (event) => {
     setData();
     displayBook();
     displayStats();
-    checkIndex();
     console.log(myLibrary);
     // @ts-ignore
     form.reset();
 });
 
-function checkIndex() {
+
+// Romve from Array and DOM
+const removeBtns = document.querySelectorAll('.remove');
+
+removeBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        let card = button.parentElement;
+        // @ts-ignore
+        // button.parentElement.dataset.index = myLibrary.indexOf();
+        let bookIndex = parseInt(button.parentElement.dataset.index);
+        myLibrary.splice(bookIndex, 1);
+        displayStats();
+        displayBook();
+        console.log(myLibrary);
+    });
+});
+
+function displayStats() {
+    let myLibraryLength = myLibrary.length;
+    let myReadBooks = 0;
+    let myUnreadBooks = 0;
     myLibrary.forEach(function (element) {
-        element.index = myLibrary.indexOf(element);
+        if(element.read === "Read") {
+            myReadBooks++;
+        } else {
+            myUnreadBooks++;
+        }
+    })
+    const readBooks = document.querySelector("#read");
+    readBooks.textContent = `Read: ${myReadBooks}`;
+    const unreadBooks = document.querySelector("#unread");
+    unreadBooks.textContent = `Unread: ${myUnreadBooks}`;
+    const totalBooks = document.querySelector("#total");
+    totalBooks.textContent = `Total: ${myLibraryLength}`;
+}
+
+displayStats();
+
+const popup = document.querySelector(".popup");
+
+function formPopup() {
+    // @ts-ignore
+    popup.style.display = "block"
+}
+
+window.onclick = function (event) {
+    if(event.target == popup) {
+        // @ts-ignore
+        popup.style.display = "none";
+    }
+}
+
+// Working on this
+const readBtn = document.querySelector(".read")
+readBtn.addEventListener("click", () => {
+    readBtn.textContent = "Not Read";
+})
+
+function clearData() {
+    let divs = document.querySelectorAll(".card");
+    divs.forEach(function (div) {
+        div.remove;
     })
 }
 
-
-const remove = document.querySelector('.remove');
-
-remove.addEventListener('click', (event) => {
-    // card.dataset.index = myLibrary.indexOf(element);
-})
+function serialize() {
+    let chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+        serialLength = 10,
+        randomSerial = "", i, randomNumber;
+    for(i = 0; i < serialLength; i = i + 1) {
+        randomNumber = Math.floor(Math.random() * chars.length);
+        randomSerial += chars.substring(randomNumber, randomNumber + 1);
+    }
+    return randomSerial;
+}
 
 // Set Data
 function setData() {
     localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
 }
-
