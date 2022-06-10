8a82443a247f1413d649b384174d459ab398ef19
Jose Gutierrez <josefgg58@gmail.com>
2022-06-09 21:14:34 -0500

    Read toggle and corrected general functionality

--- js/main.js
+++ js/main.js
@@ -1,29 +1,36 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, serial) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.serial = serial;
 }
 
 Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
 }
 
+Book.prototype.toggle = function () {
+    if(this.read == "Not Read") {
+        this.read = "Read";
+    }
+    else if(this.read = "Read") {
+        this.read = "Not Read";
+    }
+}
 
 
-function addBookToLibrary(title, author, pages, read) {
-    let x = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, serial) {
+    let x = new Book(title, author, pages, read, serial);
     myLibrary.push(x);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien.", "295", "Read");
-addBookToLibrary("The Hobbit2", "J.R.R. Tolkien.", "295", "Read");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien.", "295", "Read");
-addBookToLibrary("The Hobbit2", "J.R.R. Tolkien.", "295", "Read");
-
-
+addBookToLibrary("The Hobbit1", "J.R.R. Tolkien.", "295", "Read", "hjgh3jqg2");
+addBookToLibrary("The Hobbit2", "J.R.R. Tolkien.", "295", "Not Read", "zefefee3g2");
+addBookToLibrary("The Hobbit3", "J.R.R. Tolkien.", "295", "Read", "dsffsd33ef");
+addBookToLibrary("The Hobbit4", "J.R.R. Tolkien.", "295", "Read", "ww7G86s6fy");
 
 const container = document.querySelector("#books-grid");
 const cards = document.querySelectorAll(".book-card")
@@ -47,12 +54,13 @@ function displayBook() {
         const card = document.createElement('div');
         card.classList.add('book-card');
         // @ts-ignore
-        card.dataset.index = myLibrary.indexOf(element);
+        // card.dataset.index = myLibrary.indexOf(element); 
+        card.dataset.index = element.serial;
         container.insertBefore(card, container.firstChild);
         const data = document.createElement("p");
         data.classList.add("data");
         card.appendChild(data);
-        data.textContent = `DOM: ${card.dataset.index} | Array: ${myLibrary.indexOf(element)}`
+        data.textContent = `DOM: ${card.dataset.index} | Array: ${element.serial}`
         const title = document.createElement("p");
         title.classList.add("title");
         title.textContent = element.title;
@@ -78,6 +86,7 @@ function displayBook() {
 
 displayBook();
 
+
 const form = document.querySelector("#new-book-form");
 const submit = document.querySelector("#submit");
 const header = document.querySelector("#header-container");
@@ -100,35 +109,43 @@ form.addEventListener('submit', (event) => {
         // @ts-ignore
         form.elements['read'].value = "Not Read";
     }
+    let serial = serialize();
     // @ts-ignore
     let read = form.elements['read'].value;
-    let newBook = new Book(title, author, pages, read);
+    let newBook = new Book(title, author, pages, read, serial);
     console.log(newBook);
     myLibrary.push(newBook);
     setData();
     displayBook();
+    removeCard();
     displayStats();
     console.log(myLibrary);
     // @ts-ignore
     form.reset();
 });
 
+function removeCard() {
 
-// Romve from Array and DOM
-const removeBtns = document.querySelectorAll('.remove');
-
-removeBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-        let card = button.parentElement;
-        // @ts-ignore
-        // button.parentElement.dataset.index = myLibrary.indexOf();
-        let bookIndex = parseInt(button.parentElement.dataset.index);
-        myLibrary.splice(bookIndex, 1);
-        displayStats();
-        displayBook();
-        console.log(myLibrary);
+    const removeBtns = document.querySelectorAll('.remove');
+    removeBtns.forEach((button) => {
+        button.addEventListener('click', () => {
+            // @ts-ignore
+            // button.parentElement.dataset.index = myLibrary.indexOf();
+            let bookIndex = button.parentElement.dataset.index;
+            myLibrary.forEach((element) => {
+                if(element.serial === bookIndex) {
+                    myLibrary.splice(myLibrary.indexOf(element), 1);
+                }
+            })
+            button.parentElement.remove();
+            displayStats();
+            // displayBook();
+            console.log(myLibrary);
+        });
     });
-});
+}
+
+removeCard();
 
 function displayStats() {
     let myLibraryLength = myLibrary.length;
@@ -152,7 +169,6 @@ function displayStats() {
 displayStats();
 
 const popup = document.querySelector(".popup");
-
 function formPopup() {
     // @ts-ignore
     popup.style.display = "block"
@@ -165,18 +181,32 @@ window.onclick = function (event) {
     }
 }
 
-// Working on this
-const readBtn = document.querySelector(".read")
-readBtn.addEventListener("click", () => {
-    readBtn.textContent = "Not Read";
+const header2 = document.querySelector("#header-container")
+header2.addEventListener("click", (event) => {
+    // @ts-ignore
+    header2.style.backgroundColor = "blue";
 })
 
-function clearData() {
-    let divs = document.querySelectorAll(".card");
-    divs.forEach(function (div) {
-        div.remove;
+// Working on this
+const readBtn = document.querySelectorAll(".read");
+// @ts-ignore
+readBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        if(button.textContent == "Not Read") {
+            button.textContent = "Read";
+        }
+        else if(button.textContent == "Read") {
+            button.textContent = "Not Read";
+        }
+        let bookIndex = button.parentElement.dataset.index;
+        myLibrary.forEach((element) => {
+            if(element.serial === bookIndex) {
+                element.toggle();
+                console.log(element);
+            }
+        })
     })
-}
+})
 
 function serialize() {
     let chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
